79c2576805478b78b6f8e34c8d250b373c4f5560
Sharmin Khan <skhan@webtogo.de>
2020-10-10 21:01:19 +0200

    pod install

--- Offers/Offers/Controller/ViewController.m
+++ Offers/Offers/Controller/ViewController.m
@@ -7,7 +7,8 @@
 
 #import "ViewController.h"
 #import <AdSupport/ASIdentifierManager.h>
-#import <CommonCrypto/CommonDigest.h>
+#import <NSHash/NSString+NSHash.h>
+#import <NSHash/NSData+NSHash.h>
 
 @interface ViewController ()
 
@@ -26,9 +27,9 @@
 
 - (IBAction)getOffersButtonTap:(id)sender {
     
-    NSString *appId = [NSString stringWithFormat:@"%s%@", "appid=", self.appIdTextField.text];
-    NSString *userId = [NSString stringWithFormat:@"%s%@", "&uid=", self.userIdTextField.text];
-    NSString *securityToken = self.securityTokenTextField.text;
+    NSString *appId = [NSString stringWithFormat:@"%s%s", "appid=", "2070"];
+    NSString *userId = [NSString stringWithFormat:@"%s%s", "&uid=", "superman"];
+    NSString *securityToken = [NSString stringWithFormat:@"%s", "1c915e3b5d42d05136185030892fbb846c278927"]; //self.securityTokenTextField.text;
     
     NSString *ip = [NSString stringWithFormat:@"%s%s", "&ip=", "109.235.143.113"];
    
@@ -36,7 +37,8 @@
     
     NSDate *date = [NSDate date];
     NSTimeInterval timeStampInterval = [date timeIntervalSince1970];
-    NSString *timestamp = [NSString stringWithFormat:@"%s%f", "&timestamp=", timeStampInterval];
+    
+    NSString *timestamp = [NSString stringWithFormat:@"%s%@", "&timestamp=", [NSString stringWithFormat:@"%lu", (long)[[NSNumber numberWithDouble:[[NSDate date] timeIntervalSince1970]] integerValue]]];
     
     NSString *offerTypes = [NSString stringWithFormat:@"%s%s", "&offer_types=", "112"];
     
@@ -49,34 +51,23 @@
     
     NSString *idfaEnabled = [NSString stringWithFormat:@"%s%s", "&apple_idfa_tracking_enabled=", "true"];
     
-    NSString *googleParameters = [NSString stringWithFormat:@"%s", "&google_ad_id=a0b0c0d0-a0b0-c0d0-e0f0-a0b0c0d0e0f0&google_ad_id_limited_tracking_enabled=true"];
-    
     NSString *apiKey = [NSString stringWithFormat:@"%@", securityToken];
     
     
-    NSString *gatheredParameters = [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@", appId, userId, ip, locale, timestamp, offerTypes, version, apple_idfa, idfaEnabled, googleParameters, apiKey ];
+    NSString *gatheredParameters = [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@", appId, userId, ip, locale, timestamp, offerTypes, version, apple_idfa, idfaEnabled, apiKey ];
     
     NSLog(@"gathered: %@",gatheredParameters);
     
-    NSString *hashKey = [NSString stringWithFormat:@"%s%@", "&hashkey=", [self digest:gatheredParameters]];
+    NSString *hashKey = [NSString stringWithFormat:@"%s%@", "&hashkey=", [gatheredParameters SHA1]];
     
     NSLog(@"hashkey: %@",hashKey);
     
-    NSString *urlParameters = [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@%@", baseUrl, appId, userId, ip, locale, timestamp, offerTypes, version, apple_idfa, idfaEnabled, googleParameters, hashKey ];
+    NSString *urlParameters = [NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@", baseUrl, appId, userId, ip, locale, timestamp, offerTypes, version, apple_idfa, idfaEnabled, hashKey ];
     
     NSLog(@"url: %@",urlParameters);
 
 }
 
--(NSString*) digest:(NSString*)input {
-  NSData *data = [input dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-  uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-  CC_SHA1(data.bytes, data.length, digest);
-  NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
-  for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
-    [output appendFormat:@"02x", digest[i]];
-  return output;
-}
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     self.appIdTextField.resignFirstResponder;
