fa71458e44fedf5ffc813a8370cf153e1ac485a1
Sharmin Khan <skhan@webtogo.de>
2020-10-10 16:54:38 +0200

    Setting up first VC for user data

---
+++ Offers/Offers/Model/OfferModel.h
@@ -0,0 +1,13 @@
+//
+//  OfferModel.h
+//  Offers
+//
+//  Created by Sharmin Khan on 10.10.20.
+//
+
+#ifndef OfferModel_h
+#define OfferModel_h
+
+
+#endif /* OfferModel_h */
+
---
+++ Offers/Offers/Model/OfferModel.m
@@ -0,0 +1,65 @@
+//
+//  OfferModel.m
+//  Offers
+//
+//  Created by Sharmin Khan on 10.10.20.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//struct OfferType {
+//    int offer_type_id;
+//    NSString *readable;
+//};
+//
+//struct Thumbnail {
+//    NSString *lowres;
+//    NSString *hires;
+//};
+//
+//struct TimeToPayput {
+//    int amount;
+//    NSString *readable;
+//};
+//
+//struct Offer {
+//    NSString *title;
+//    NSString *offer_id;
+//    NSString *teaser;
+//    NSString *required_actions;
+//    NSString *link;
+//    //NSArray<struct OfferType *> *offer_types;
+//    struct Thumbnail thumbnail;
+//    int   payout;
+//    struct TimeToPayput time_to_payput;
+//};
+
+
+
+//{
+//    "title": "Tap  Fish",
+//    "offer_id": 13554,
+//    "teaser": "Download and START" ,
+//    "required_actions": "Download and START",
+//    "link": "http://iframe.fyber.com/mbrowser?appid=157&lpid=11387&uid=player1",
+//    "offer_types" : [
+//     {
+//      "offer_type_id": 101,
+//      "readable": "Download"
+//     },
+//     {
+//      "offer_type_id": 112,
+//      "readable": "Free"
+//     }
+//    ] ,
+//    "thumbnail" : {
+//     "lowres": "http://cdn.fyber.com/assets/1808/icon175x175-2_square_60.png",
+//     "hires": "http://cdn.fyber.com/assets/1808/icon175x175-2_square_175.png"
+//    },
+//    "payout": 90,
+//    "time_to_payout" : {
+//     "amount": 1800 ,
+//     "readable": "30 minutes"
+//    }
+//  }
